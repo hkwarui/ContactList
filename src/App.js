8a3678395aa961c48b239529daeaa014abf8db7e
Henry Warui <warui.henry@gmail.com>
2021-08-01 00:37:27 +0300

    contact list app using random user api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import useFetch from "react-fetch-hook";
+import { useEffect, useState } from "react";
 
-function App() {
+import Navigation from "./Navigation";
+import ContactCards from "./ContactCard";
+
+const App = () => {
+  const url = "https://randomuser.me/api";
+  const { isLoading, data, error } = useFetch(url + "?results=200");
+
+  const [contactList, setContactList] = useState();
+  const [filterQuery, setFilterQuery] = useState();
+
+  useEffect(() => {
+    if (!filterQuery) {
+      setContactList(data?.results.slice(0, 12));
+    } else {
+      const queryString = filterQuery.toLowerCase();
+
+      const filteredData = data?.results.filter((contact) => {
+        const fullName = `${contact.name.first} ${contact.name.last}`;
+
+        if (queryString.length === 1) {
+          const firstLetter = fullName.charAt(0).toLowerCase();
+          return firstLetter === queryString;
+        } else {
+          return fullName.toLowerCase().includes(queryString);
+        }
+      });
+
+      setContactList(filteredData);
+    }
+  }, [data, filterQuery]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-gray-100">
+        <Navigation />
+        <section className="flex flex-wrap justify-between">
+          <form>
+            <input
+              type="text"
+              className="h-14 w-auto md:w-96 ml-20 pr-8 pl-5 mt-4 border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent rounded-md z-0 "
+              placeholder="Search contact..."
+              onChange={(event) => setFilterQuery(event.target.value)}
+            />
+          </form>
+
+          <button
+            className="bg-indigo-400 hover:bg-indigo-500 text-white font-semibold rounded xs:ml-20 mt-8 mr-8 py-2 px-4"
+            onClick={() => setContactList(data?.results)}
+          >
+            Refesh
+          </button>
+        </section>
+
+        <section className="p-2 grid sm:grid-cols-2 md:grid-cols-4 gap-6">
+          {contactList?.length < 1 && (
+            <h1 className="ml-20">No data matches your search</h1>
+          )}
+          <ContactCards contactList={contactList} />
+        </section>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Navigation.jsx
@@ -0,0 +1,138 @@
+const Navigation = () => {
+  return (
+    <>
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="flex justify-between">
+            <div class="flex space-x-7">
+              {/* website logo */}
+              <div>
+                {/* <a href="#Home" className="flex items-center py-4 ">
+                  <img
+                    className="hidden lg:block h-8 w-auto mr-2 "
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="ContactsList"
+                  ></img>
+                  <span className="font-semibold tetx-gray-500 text-lg ml-0">
+                    ContactList
+                  </span>
+                </a> */}
+
+                <a
+                  href="#home"
+                  className="flex flex-nowrap items-center py-4 px-2"
+                >
+                  <img
+                    className="h-8 mx-1  px-0 "
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Logo"
+                  />
+                  <span class="font-bold text-gray-600 text-lg">
+                    ContactList
+                  </span>
+                </a>
+              </div>
+
+              {/* Primary navbar items */}
+              <div className="hidden md:flex items-center space-x-1">
+                <a
+                  href="#about"
+                  className="py-4 px-2 text-gray-500 focus:text-indigo-500 border-b-2 border-white focus:border-indigo-500 font-semibold hover:text-indigo-500 hover:border-indigo-500 transition duration-300"
+                >
+                  About
+                </a>
+                <a
+                  href="#blog"
+                  className="py-4 px-2 text-gray-500  focus:text-indigo-500 border-b-2 border-white focus:border-indigo-500 font-semibold hover:text-indigo-500 hover:border-indigo-500 transition duration-300"
+                >
+                  Blog
+                </a>
+                <a
+                  href="#contact"
+                  className="py-4 px-2 text-gray-500 focus:text-indigo-500  border-b-2 border-white focus:border-indigo-500 font-semibold hover:text-indigo-500 hover:border-indigo-500  transition duration-300"
+                >
+                  Contact Us
+                </a>
+              </div>
+            </div>
+
+            {/* Secondary Navbar items */}
+            <div className="hidden  md:flex items-center space-x-3">
+              <a
+                href="#login"
+                className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-indigo-500 hover:text-white transition duration-300 focus:bg-indigo-500 focus:text-white"
+              >
+                Log in
+              </a>
+              <a
+                href="#signup"
+                className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-indigo-500  hover:text-white transition duration-300 focus:bg-indigo-500 focus:text-white"
+              >
+                Sign Up
+              </a>
+            </div>
+
+            {/* Mobile menu button  */}
+            <div class="md:hidden flex items-center">
+              <button className="outline-none mobile-menu-button">
+                <svg
+                  className="w-6 h-6 text-indigo-500"
+                  x-show="!showMenu"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </button>
+            </div>
+
+            {/* Mobile menu */}
+
+            <div className="hidden mobile-menu">
+              <ul class="">
+                <li class="active">
+                  <a
+                    href="index.html"
+                    class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#services"
+                    class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+                  >
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#about"
+                    class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#contact"
+                    class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+                  >
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
